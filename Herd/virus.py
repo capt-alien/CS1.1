3a375973c172d8bdaf95ab12354d18ad83de18fe
capt-alien <eric.botcher@presidio.edu>
2018-10-10 17:28:37 -0700

    passed virus pytest

---
+++ Herd/virus.py
@@ -0,0 +1,7 @@
+class Virus(object):
+
+    def __init__(self, name, mortality_rate, reproduction_rate):
+        #Initalizes Virus
+        self.name = name
+        self.mortality_rate = mortality_rate
+        self.reproduction_rate = reproduction_rate
